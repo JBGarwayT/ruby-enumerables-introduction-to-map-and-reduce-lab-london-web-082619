bdaf54fd035a5ee9e7744b75fbdb97d08658e9e3
Jean-Baptiste Garway-Templeman <jbgarwayt@gmail.com>
2019-08-05 15:21:15 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -39,8 +39,8 @@ def reduce_to_total (array, starting_value = 0)
   counter = 0
   new_value = starting_value
   while counter < array.length
-  new_value = new_value + array[counter]
-  counter = counter + 1
+  new_value += array[counter]
+  counter += 1
 end
 new_value
 end
