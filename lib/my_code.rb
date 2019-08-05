0528c82b40cee1eb7efb6fd39caf54c2047e4774
Jean-Baptiste Garway-Templeman <jbgarwayt@gmail.com>
2019-08-05 15:06:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,59 @@
-# My Code here....
+# Map_like methods
+def map_to_negativize (array)
+  counter = 0
+  new_array = []
+  while counter < array.length
+  new_array << array[counter] * -1
+  counter = counter + 1
+  end
+new_array
+end
+
+def map_to_no_change (array)
+  array
+end
+
+def map_to_double (array)
+  counter = 0
+  new_array = []
+  while counter < array.length
+  new_array << array[counter] * 2
+  counter = counter + 1
+end
+new_array
+end
+
+def map_to_square (array)
+  counter = 0
+  new_array = []
+  while counter < array.length
+  new_array << array[counter] ** 2
+  counter = counter + 1
+end
+new_array
+end
+
+
+#Reduce_like methods
+def reduce_to_total (array, element)
+  counter = 0
+  new_value = element + 0
+  while counter < array.length
+  new_value = new_value + array[counter]
+  counter = counter + 1
+end
+new_value
+end
+
+def reduce_to_all_true (array)
+  counter = 0
+  while counter < array.length
+  counter = counter + 1
+  if array = false
+     return !array
+  else
+    return !!array
+end
+end   
+end
+
